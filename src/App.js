49aa85f2ad282a08fd02dd53f17a929025ddd60e
Jessi Ijelman Lipovetzky <46876160@est.ort.edu.ar>
2023-05-08 15:59:59 -0300

    css

--- src/App.js
+++ src/App.js
@@ -2,21 +2,20 @@ import React from 'react'
 
 import { Navbar, Bloque } from './componentes';
 import { Camara, Form, Header, Historial, Info } from './contenedores';
+import './app.css';
 
 const App = () => {
   return (
     <div className="App">
-        <div>
+        <div className="gradient__bg">
           <Navbar />
           <Header />
         </div>
-        <div>
-          <Bloque />
-          <Camara />
-          <Form />
-          <Historial />
-          <Info />
-        </div>
+        <Bloque />
+        <Camara />
+        <Form />
+        <Historial />
+        <Info />
     </div>
   )
 }
--- src/componentes/navbar/navbar.jsx
+++ src/componentes/navbar/navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri'
 import './navbar.css'
+/*import logo from '../../assets/lpgp.svg'*/
 
 const Navbar = () => {
   return (
     <div>
-        Navbar
+        <div className="navbar">
+          <div className="navbar-links">
+            <div className="navbar-links_logo">
+              {/* img src={logo} alt "logo" */}
+            </div>
+          </div>
+        </div>
     </div>
   )
 }
