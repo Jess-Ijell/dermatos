236d7a296bf444ccc2d37731f2adb5d8dafb4bc5
Jessi Ijelman Lipovetzky <46876160@est.ort.edu.ar>
2023-05-22 16:12:25 -0300

    header

--- src/contenedores/header/header.jsx
+++ src/contenedores/header/header.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './header.css';
+// import pics from '../../assets/pics.png'
+// import ai from '../../assets/ai.png'
 
 const Header = () => {
   return (
-    <div>
+    <div className="_header section_padding" id="home">
+      <div className="_header-content">
+        <h1 /* className="gradient_text" */> Dermatos: an app to dectct melanomas </h1>
+        <p>tilizamos inteligencia artificial entrenada para detectar el porcentaje de probabilidad de que un lunar sea un melanoma.</p>
+
+        <div className="_header-content_input">
+          <input type="email" placeholder="Your Email Adress" />
+          <button type="button">ANALISIS</button>
+        </div>
+
+        {/* <div className="_header-content_pics">
+          <img src={pics} alt="pics" />
+          <p> aca iría texto bla bla bla bla bla bla blablablablablablablabla </p>
+        </div> 
+        <div className="header-image">
+          <img src={ai} alt="ai"/>
+        </div> */}
+
+      </div>
     </div>
   )
 }
